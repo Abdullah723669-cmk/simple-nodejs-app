1f7d8bed2ac8bd2f184fdf5085f5ab1a8a06038f
root <root@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-01 18:11:00 +0000

    cicd test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(<h1 style="background-color:orange">'Hello, World!--from Mamun DevOps School!')</h1>;
 });
 
 module.exports = app;

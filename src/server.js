127f1f7b259e6252b25b3c97027c089fe3b486d2
root <root@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-01 17:11:04 +0000

    Ad mamun

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello dear ---mamun--- from Node.js CI/CD app! -- MAMUN DEVOPS CENTER< joydebpur//GAZIPUR\n');
+  res.end('Hello dear ---Mamun--Mia- from Node.js CI/CD app! -- MAMUN DEVOPS CENTER< joydebpur//GAZIPUR\n');
 });
 
 server.listen(PORT, () => {

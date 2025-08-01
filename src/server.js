23f0285439f85cd38224c7df703bf2692367718e
root <root@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-01 18:18:42 +0000

    cut excess

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!--from Mamun DevOps School!');
+  res.send('Hello, World!');
 });
 
 module.exports = app;

9856638acb8fe30c8139fab3a1f01340f8f6256f
EC2 Default User <ec2-user@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-02 02:21:27 +0000

    change sentence

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../src/server');
 
 describe('GET /', () => {
-  it('should return Hello, World!', async () => {
+  it('should return Hello, World! -- How are you??', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello, World!');
+    expect(res.text).toBe('Hello, World! -- How are you??');
   });
 });

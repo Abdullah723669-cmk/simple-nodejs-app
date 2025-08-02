6d019022cfe9d25c5087c38a27bb5a455225d656
root <root@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-02 10:16:10 +0000

    Faiza

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! -- How are you,BOBI??');
+  res.send('Hello, World! -- How are you, all BOBI? Where is Faiza?');
 });
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../src/server');
 
 describe('GET /', () => {
-  it('should return Hello, World! -- How are you,BOBI??', async () => {
+  it('should return Hello, World! -- How are you, all BOBI? Where is Faiza?', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello, World! -- How are you,BOBI??');
+    expect(res.text).toBe('Hello, World! -- How are you, all BOBI? Where is Faiza?');
   });
 });

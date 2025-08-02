85a237de7c84189562dedd36d0775d2d11514213
root <root@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-02 06:20:49 +0000

    word add-BOBI

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../src/server');
 
 describe('GET /', () => {
-  it('should return Hello, World! -- How are you??', async () => {
+  it('should return Hello, World! -- How are you,BOBI??', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello, World! -- How are you??');
+    expect(res.text).toBe('Hello, World! -- How are you,BOBI??');
   });
 });

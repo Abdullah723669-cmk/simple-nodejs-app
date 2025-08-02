f3be9274ea77a043918ce5b4c9ff76121302169e
EC2 Default User <ec2-user@ip-172-31-20-201.eu-north-1.compute.internal>
2025-08-02 02:47:59 +0000

    ass

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! -- How are you??');
 });
 
 module.exports = app;
